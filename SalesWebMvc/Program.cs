c389417477f6026d0ceecd4d8b0982f15b4e3b5b
Jorge Machado <miguelj1992@outlook.com>
2022-09-20 16:32:55 +0100

    Create de DataBase

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,14 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+using System.Configuration;
 
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+
+// Obter a connectionString a partir da nossa classe de Context
+string MyConnectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
+
+// Registar a Connection String nos Services do Programa
+//A ultima string utilizada é o nome do projeto
+builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+           options.UseMySql(MyConnectionString, ServerVersion.AutoDetect(MyConnectionString),p=> p.MigrationsAssembly("SalesWebMvc"))); 
+
+
+/* builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+*/
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
         var app = builder.Build();
+
+        // Teste para adicionar um serviço
+        // Para adicionar um serviço no .NetFramework 6, é  so utilizar a palavra 
+        // builder.service....
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
@@ -18,6 +36,7 @@ using SalesWebMvc.Data;
             app.UseHsts();
         }
 
+        // Aqui ficava a parte do Configure, no 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
 
