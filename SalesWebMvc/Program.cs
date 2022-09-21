fb9912101cfd9f6243dd1071cbfdf7e6f643f6c6
Jorge Machado <miguelj1992@outlook.com>
2022-09-21 10:58:28 +0100

    Populate the database

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -3,26 +3,44 @@ using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
 using System.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+       
 
-// Obter a connectionString a partir da nossa classe de Context
-string MyConnectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
+        // Obter a connectionString a partir da nossa classe de Context
+        string MyConnectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
 
-// Registar a Connection String nos Services do Programa
-//A ultima string utilizada é o nome do projeto
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-           options.UseMySql(MyConnectionString, ServerVersion.AutoDetect(MyConnectionString),p=> p.MigrationsAssembly("SalesWebMvc"))); 
-
-
-/* builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
-*/
+        // Registar a Connection String nos Services do Programa
+        //A ultima string utilizada é o nome do projeto
+        builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+                   options.UseMySql(MyConnectionString, ServerVersion.AutoDetect(MyConnectionString), p => p.MigrationsAssembly("SalesWebMvc")));
 
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+        /* builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+        */
+
+        // Registar a nossa classe(serviço) SeedingService, injeta o serviço no sistema de dependencia da aplicaçao
+        builder.Services.AddScoped<SeedingService>();
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
 
         var app = builder.Build();
+
+
+
+        /// Isto é usado para popular a Base de dados, passamos como parametro do metodo um objeto
+        ///IServiceProvider, e na classe SeedingServices, declaramos o metodo estático
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            SeedingService.Seed(services);
+        }
+
 
         // Teste para adicionar um serviço
         // Para adicionar um serviço no .NetFramework 6, é  so utilizar a palavra 
@@ -35,6 +53,7 @@ builder.Services.AddControllersWithViews();
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+
 
         // Aqui ficava a parte do Configure, no 
         app.UseHttpsRedirection();
@@ -49,3 +68,5 @@ builder.Services.AddControllersWithViews();
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
+    }
+}
